830aed0ddaf8f81b59223cdcc9d8032b4964af3b
SaadShafiq-khawaja <saadshafiq2019@gmail.com>
2026-01-20 23:45:47 +0500

    simple oop class and object

---
+++ basics.php
@@ -0,0 +1,26 @@
+<?php
+
+class calculation{
+    public $a, $b,$c;
+
+    function sum(){
+        $this -> c = $this->a + $this->b;
+        return $this->c;
+    }
+    function sub(){
+        $this -> c = $this->a - $this->b;
+        return $this->c;
+    }
+}
+
+$c1 = new calculation();
+$c1->a = 20;
+$c1->b = 10;
+
+$c2 = new calculation();
+$c2->a = 50;
+$c2->b = 25;
+
+echo "Sum of calulations is " . $c1->sum() . "\n";
+echo "Subtract of calulations is " . $c2->sub() . "\n";
+?>
